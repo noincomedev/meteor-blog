6efdf2a301a4d7e8c159a64d9a095147b3c9393c
Diego Robles <diego@hemployeed.com>
2018-05-01 21:19:21 -0300

    added user api

---
+++ imports/startup/server/api/index.js
@@ -0,0 +1,16 @@
+import { createApolloServer } from "meteor/apollo";
+import { makeExecutableSchema } from "graphql-tools";
+import merge from "lodash/merge";
+
+import UserSchema from "../../../api/user/User.graphql";
+import UserResolvers from "../../../api/user/resolvers";
+
+// a
+
+const typeDefs = [UserSchema];
+
+const resolvers = merge(UserResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+createApolloServer({ schema });
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,6 +1,8 @@
 import { Meteor } from "meteor/meteor";
 
 import "./accounts/config";
+import "./api";
+
 Meteor.startup(() => {
   console.log("server started");
 });
