7628b65061a1643f9a10458901d491bfe42907ff
Diego Robles <diego@hemployeed.com>
2018-07-09 12:45:34 -0400

    fixed private postlist behaviour

--- imports/ui/components/post/list/Private.jsx
+++ imports/ui/components/post/list/Private.jsx
@@ -26,39 +26,25 @@ const styles = theme => ({
 });
 
 class Private extends Component {
-  state = {
-    showButton: false,
-    showForm: true
-  };
-
-  toggleButton = () => {
-    this.setState({
-      showButton: !this.state.showButton,
-      showForm: !this.state.showForm
-    });
-  };
-
-  toggleForm = () => {
-    this.setState({ showForm: !this.state.showForm });
-  };
-
-  toggleCancel = () => {
-    this.setState({
-      showButton: !this.state.showButton,
-      showForm: !this.state.showForm
-    });
-  };
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    return {
-      showButton: nextProps.posts.length > 0 ? true : false,
-      showForm: nextProps.posts.length > 0 ? false : true
-    };
+  constructor(props) {
+    super(props);
+    const { posts } = props;
+    this.state = { controls: posts.length > 0 };
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.posts.length > 0 && this.props.posts.length == 0) {
+      this.toggleControls();
+    }
+  }
+
+  toggleControls = () => {
+    this.setState({ controls: !this.state.controls });
+  };
+
   render() {
-    const { classes } = this.props;
-    const { showButton, showForm } = this.state;
+    const { classes, posts } = this.props;
+    const { controls } = this.state;
     return (
       <Grid container className={classes.container} justify="center">
         <Grid container alignItems="center" className={classes.container}>
@@ -72,11 +58,11 @@ class Private extends Component {
             xs={4}
             style={{ display: "flex", flex: 1, justifyContent: "flex-end" }}
           >
-            {showButton && (
+            {controls && (
               <Button
                 variant="flat"
                 color="secondary"
-                onClick={this.toggleButton}
+                onClick={this.toggleControls}
                 className={classes.button}
               >
                 +
@@ -87,22 +73,24 @@ class Private extends Component {
         <Grid item xs={12}>
           <Divider />
         </Grid>
-        {showForm && (
+        {!controls && (
           <Grid item xs={12} style={{ paddingRight: 8 }}>
             <Helmet>
               <title>NOINCOMEDEV | Add Post</title>
               <meta name="Add Post" content="Add Post" />
             </Helmet>
             <CardWithTitleAndContent title="New Post">
-              <PostForm handleCancel={this.toggleCancel} />
+              <PostForm handleCancel={this.toggleControls} />
             </CardWithTitleAndContent>
           </Grid>
         )}
-        {this.props.posts.map(post => (
-          <Grid item xs={12} key={post._id}>
-            <PrivatePostItem post={post} />
-          </Grid>
-        ))}
+        {controls &&
+          posts.length > 0 &&
+          posts.map(post => (
+            <Grid item xs={12} key={post._id}>
+              <PrivatePostItem post={post} />
+            </Grid>
+          ))}
       </Grid>
     );
   }
